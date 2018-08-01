b462e806e663a9c25e8ce46efbd070e579429be1
Muhammad Usman <usman@debo.edu>
2018-08-01 16:13:04 +0500

    month

--- winds/src/pd.py
+++ winds/src/pd.py
@@ -6,12 +6,17 @@ import matplotlib.pyplot as plt
 
 wind_data = pd.read_csv(sys.argv[1]);
 
-
 wind_data.index = wind_data['time'].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%d %H:%M'))
 daily = wind_data.resample('D').mean()
+print(daily.info)
+daily_month = wind_data.resample('m').mean()
+print(daily_month)
 
-daily.info()
-
+#daily.sort_values( by='t5_temperature_mean' )
+#print(daily['t5_temperature_mean'])
 plt.figure()
-daily.plot( y=['a20_wind_speed_mean', 'a40_wind_speed_mean', 'a60_wind_speed_mean', 'a80T_wind_speed_mean'], kind='line', linewidth=0.5)
+daily.plot(y='a20_wind_speed_mean', kind='line', linewidth=0.5)
+plt.show()
+plt.figure()
+daily_month.plot(y='a20_wind_speed_mean', kind='line', linewidth=0.5)
 plt.show()
